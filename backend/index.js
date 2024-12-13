58b155a364316c016ebeb3c6bc63b4cc80d98b7b
param078 <pramanandupadhyay2@gmail.com>
2024-12-13 22:54:01 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+const {createTodo, updateTodo} = require('./type')
 const app = express();
 
 app.use(express.json());
@@ -9,15 +9,35 @@ res.send("hey there");
 })
 
 app.post('/todo', () =>{
+const createPayload = req.body;
+const parsePayload = createTodo;
 
+if(!parsePayload.success){
+    res.status(411).json({
+        msg : "You have sent a wrong input",
+    })
+    return;
+}
+else{
+    //put it in mongoDB
+}
 })
 
 app.put('/completed', () =>{
+const updatePayload = req.body;
+const parsePayload = updateTodo.safeParse(updatePayload);
 
+if(!parsePayload.success){
+    res.status(411).json({
+        msg : "You have sent a wrong input",
+    })
+    return;
+}
+else{
+    
+}
 })
 
-app.delete('/', () =>{
 
-})
 
 app.listen(5000);
